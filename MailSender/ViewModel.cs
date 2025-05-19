1162540b51fb93b7bc1ad19d8e780da6040fe9bf
dmitriygorchak <129514494+dmitriygorchak@users.noreply.github.com>
2025-05-19 13:45:22 +0300

    Add project files.

---
+++ MailSender/ViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender
+{
+    public class ViewModel
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Theme { get; set; }
+        public string Body { get; set; }
+        private ObservableCollection<string> attachments;
+        public IEnumerable<string> Attachments => attachments;
+        public ViewModel(string from)
+        {
+            From = from;  
+            attachments = new ObservableCollection<string>();
+        }
+        public void AddAttach(string at)
+        {
+           attachments.Add(at);
+        }
+
+    }
+}
