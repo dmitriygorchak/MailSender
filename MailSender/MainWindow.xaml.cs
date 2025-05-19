539388f4a21b3abb6268b5f57f8a9eaf4e781608
dmitriygorchak <129514494+dmitriygorchak@users.noreply.github.com>
2025-05-19 13:52:02 +0300

    2 stage main window sender

--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -39,5 +39,42 @@ namespace MailSender
             important = false;
             this.DataContext = model;
         }
+
+        private void Attach_Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.ShowDialog();
+            model.AddAttach(dialog.FileName);
+        }
+
+        private void Send_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MailMessage message = new MailMessage(model.From, model.To, model.Theme, model.Body);
+            foreach (var item in model.Attachments)
+            {
+                message.Attachments.Add(new Attachment(item));
+            }
+            if (important)
+                message.Priority = MailPriority.High;
+
+            SmtpClient client = new SmtpClient(server, port);
+            client.EnableSsl = true;
+
+            client.Credentials = new NetworkCredential(user_name, user_pass);
+            client.SendCompleted += Client_SendCompleted;
+
+            client.SendAsync(message, message);
+        }
+
+        private void Client_SendCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            var state = (MailMessage)e.UserState;
+            MessageBox.Show($"Message was sent! Subject: {state.Subject}!");
+        }
+
+        private void Mark_Important_Button_Click(object sender, RoutedEventArgs e)
+        {
+            important = true;
+        }
     }
 }
